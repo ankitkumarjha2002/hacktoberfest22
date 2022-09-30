669353139abd41738ad0620c31ea20b8b56dbc24
ankitkumarjha2002 <114497198+ankitkumarjha2002@users.noreply.github.com>
2022-10-01 01:39:29 +0530

    Pointer
    Complete the update function in the editor below.
    
    update has the following parameters:
    
    int *a: an integer
    int *b: an integer
    Returns
    
    The function is declared with a void return type, so there is no value to return. Modify the values in memory so that  contains their sum and  contains their absoluted difference.
    
    
    Input Format
    
    Input will contain two integers,  and , separated by a newline.
    
    Sample Input
    
    4
    5
    Sample Output
    
    9
    1

---
+++ Pointer.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a,b,sum,diff;
+    cin>>a>>b;
+    sum=a+b;
+    diff=abs(a-b);
+    cout<<sum;
+    cout<<"\n";
+    cout<<diff;
+}
